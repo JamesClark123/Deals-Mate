f09f76645196e64b63c30b39a23b803a6c6253d9
James Clark <jamesloganclark@gmail.com>
2020-07-28 18:36:52 -0700

    fix small typing error

--- server/src/config/prod.js
+++ server/src/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  TOKEN_SECRET: process.env.SECRET,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
