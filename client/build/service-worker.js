0419e30bf16b9b5740b8fae64b8f4f544efca997
James Clark <jamesloganclark@gmail.com>
2020-08-16 21:28:29 -0700

    small package change

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4f3b8d70cc868a6a4c34530a6f1b5b6.js"
+  "/precache-manifest.47548e02abe0ffbdcfbdc26f34118d3c.js"
 );
 
 self.addEventListener('message', (event) => {
